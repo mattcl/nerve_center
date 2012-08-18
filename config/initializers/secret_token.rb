ebc5db6e20cf155425d4f68e0ba42889b424e537
Matt Chun-Lum <mchunlum@gmail.com>
2012-08-17 21:15:05 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NerveCenter::Application.config.secret_token = 'ebb1f7363e1cdde4dab8f111b4ffae0ef82c0d6ba8d405b183d97243459c1fb999e020d6d4f3629cf9051c69717285f51139c5f2b8e347d782ea9929fbc9540a'
